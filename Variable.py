896ff1fdbcb1bac4eb6eb43c41bdf6e7675f4ff6
Jian-An Wang <facebook0983481181@gmail.com>
2023-01-17 15:56:03 +0800

    feat: define common variable

---
+++ Variable.py
@@ -0,0 +1,6 @@
+import os
+
+main_path = r"D:\OneDrive - tmu.edu.tw\AI_Competition\Learning Equality - Curriculum Recommendations"
+raw_content_data_path = os.path.join(main_path, "raw_data", "content.csv")
+raw_topics_data_path = os.path.join(main_path, "raw_data", "topics.csv")
+raw_correlation_data_path = os.path.join(main_path, "raw_data", "correlations.csv")
