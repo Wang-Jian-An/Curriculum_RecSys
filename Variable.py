4cf164febd52bddf40303eaf23e4ed85f329f289
Jian-An Wang <facebook0983481181@gmail.com>
2023-01-20 19:20:12 +0800

    refactor: change main path

--- Variable.py
+++ Variable.py
@@ -1,6 +1,6 @@
 import os
 
-main_path = r"E:\OneDrive - tmu.edu.tw\AI_Competition\Learning Equality - Curriculum Recommendations"
+main_path = r"D:\OneDrive - tmu.edu.tw\AI_Competition\Learning Equality - Curriculum Recommendations"
 raw_content_data_path = os.path.join(main_path, "raw_data", "content.csv")
 raw_topics_data_path = os.path.join(main_path, "raw_data", "topics.csv")
 raw_correlation_data_path = os.path.join(main_path, "raw_data", "correlations.csv")
