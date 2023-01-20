aeb7efbfbc66ee4535cf487b6411d3e7f4e09c4b
Jian-An Wang <facebook0983481181@gmail.com>
2023-01-20 19:18:54 +0800

    feat: finish one hot for conttent ids

--- 3-Define_Target.py
+++ 3-Define_Target.py
@@ -17,24 +17,15 @@ def load_content_and_correlation_data():
     raw_correlation_data = pd.read_csv(raw_correlation_data_path)
     return raw_content_data, raw_correlation_data
 
-def one_hot_encoding_format_for_correlation(target_list):
-
-    return_list = init_list.copy()
-    for i in target_list:
-        return_list[unique_contents_id.index(i)] = 1
-    return return_list
-
 if __name__ == "__main__":
     raw_content_data, raw_correlation_data = load_content_and_correlation_data()
 
     unique_contents_id = raw_content_data["id"].unique().tolist()
 
     raw_correlation_data["content_ids"] = raw_correlation_data.copy()["content_ids"].apply(lambda x: x.split(" "))
-
-    init_list = [0] * unique_contents_id.__len__()
     
     result = {
-        one_data["topic_id"]: one_hot_encoding_format_for_correlation(one_data["content_ids"]) for _, one_data in raw_correlation_data.iterrows()
+        one_data["topic_id"]: tuple([1 if i in one_data["content_ids"] else 0 for i in unique_contents_id]) for _, one_data in raw_correlation_data.iterrows()
     }
 
     # 儲存關係結果為 JSON 檔案
