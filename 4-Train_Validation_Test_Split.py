23e6c2b64f893421245a729e65d21cc0aa8cefb2
Jian-An Wang <facebook0983481181@gmail.com>
2023-01-30 15:26:26 +0800

    feat: unify setting mode of file path

--- 4-Train_Validation_Test_Split.py
+++ 4-Train_Validation_Test_Split.py
@@ -9,11 +9,12 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from Variable import *
 
-jsonFilePath = os.path.join(main_path, "raw_data", "Correlations_with_oneHotEncoding_and_SinglePairs_for_development.json.gz")
+inputJsonFilePath = os.path.join(main_path, "raw_data", "Correlations_with_oneHotEncoding_and_SinglePairs_for_development.json.gz")
+outputJsonFilePath = os.path.join(main_path, "raw_data", "SplitData_for_Correlation_for_development.json.gz")
 
 if __name__ == "__main__":
 
-    rawCorrelation = compress_json.load(jsonFilePath)
+    rawCorrelation = compress_json.load(inputJsonFilePath)
 
     unique_topics = list(rawCorrelation.keys())
 
@@ -21,11 +22,11 @@ if __name__ == "__main__":
     trainTopics, valiTopics = train_test_split(trainTopics, test_size = 0.25, shuffle = True)
     totalResult = [
         {
-            "Data-ID": 1,
+            "DataID": 1,
             "Split": splitName,
             "Topic": oneTopic,
             "Content": oneContent,
             "Correlation": correlation
         } for splitName, splitObj in zip(["train", "vali", "test"], [trainTopics, valiTopics, testTopics]) for oneTopic in tqdm.tqdm(splitObj) for oneContent, correlation in rawCorrelation[oneTopic].items()
     ]
-    compress_json.dump(pd.DataFrame(totalResult).to_dict("records"), os.path.join(main_path, "raw_data", "SplitData_for_Correlation_for_development.json.gz"))
+    compress_json.dump(pd.DataFrame(totalResult).to_dict("records"), outputJsonFilePath)
