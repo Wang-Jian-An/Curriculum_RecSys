8fd93e48db13b136a01fae41277961da02025bc7
Jian-An Wang <facebook0983481181@gmail.com>
2023-01-30 15:28:48 +0800

    feat: model training flow

--- 5-DL_training_flow.py
+++ 5-DL_training_flow.py
@@ -11,24 +11,23 @@ from Transformer_RecSys_Model import Transformer_RecSys_Model
 rawDataPath = f"{main_path}/raw_data/SplitData_for_Correlation_for_development.json.gz"
 topicDataPath = os.path.join(main_path, "raw_data", "tokenizered_topics.xlsx") 
 contentDataPath = os.path.join(main_path, "raw_data", "tokenizered_content.xlsx")
-device = "cuda" if torch.cuda.is_available else "cpu"
+# device = "cuda" if torch.cuda.is_available else "cpu"
+device = "cpu"
+print(device)
 epochs = 10
-
-def combine_input_ids_and_attention_mask_in_one_list(oneInputIds, oneAttentionMask):
-    return [
-        [i, j] for i, j in zip(oneInputIds, oneAttentionMask)
-    ]
 
 def generate_three_obj_for_train_vali_test_data(data):
     data = pd.DataFrame(data)
     return [
-        data.query("Data-ID == 1 and Split == @oneSplit") for oneSplit in ["train", "vali", "test"]
+        data.query("DataID == 1 and Split == @oneSplit") for oneSplit in ["train", "vali", "test"]
     ]
 
 def generate_TrainDataLoader(oneData: pd.DataFrame, batch_size = 128):
 
-    topicInputIdsAttentionMask = [rawTopics[oneTopic] for oneTopic in oneData["Topic"]]
-    contentInputIdsAttentionMask = [rawContent[oneContent] for oneContent in oneData["Content"]]
+    print(rawTopics[oneData["Topic"].tolist()[0]])
+    topicInputIdsAttentionMask = [rawTopics[oneTopic] for oneTopic in oneData["Topic"].tolist()]
+    print(topicInputIdsAttentionMask[0])
+    contentInputIdsAttentionMask = [rawContent[oneContent] for oneContent in oneData["Content"].tolist()]
     correlation = oneData["Correlation"].tolist()
     tensorDataset = TensorDataset(
         torch.LongTensor(topicInputIdsAttentionMask),
@@ -79,10 +78,11 @@ if __name__ == "__main__":
     rawTopics = pd.read_excel(topicDataPath)
     rawContent = pd.read_excel(contentDataPath)
     rawData = pd.DataFrame(compress_json.load(rawDataPath))
+    numContent = rawContent.shape[0]
 
     # Combine input_ids and attention mask to one list
-    rawTopics["input_ids_attention_mask"] = rawTopics.copy().apply(lambda x: combine_input_ids_and_attention_mask_in_one_list(oneInputIds = x["input_ids"], oneAttentionMask = x["attention_mask"]), axis = 1)
-    rawContent["input_ids_attention_mask"] = rawContent.copy().apply(lambda x: combine_input_ids_and_attention_mask_in_one_list(oneInputIds = x["input_ids"], oneAttentionMask = x["attention_mask"]), axis = 1)
+    rawTopics["input_ids_attention_mask"] = rawTopics.copy().apply(lambda x: [list(eval(x["input_ids"])), list(eval(x["attention_mask"]))], axis = 1)
+    rawContent["input_ids_attention_mask"] = rawContent.copy().apply(lambda x: [list(eval(x["input_ids"])), list(eval(x["attention_mask"]))], axis = 1)
 
     # convert the type of rawTopics and rawContent to json
     rawTopics = rawTopics.set_index("id")["input_ids_attention_mask"].to_dict()
@@ -92,17 +92,19 @@ if __name__ == "__main__":
     trainData, valiData, testData = generate_three_obj_for_train_vali_test_data(data = rawData)
     del rawData
     gc.collect()
+    print(trainData.shape, valiData.shape, testData.shape)
 
     # Generate DataLoader
     trainDataLoader = generate_TrainDataLoader(oneData = trainData, batch_size = 128)
     valiDataLoader, testDataLoader = [
-        generate_PredictionDataLoader(oneData = oneData, batch_size = rawContent.shape[0]) for oneData in [valiData, testData]
+        generate_PredictionDataLoader(oneData = oneData, batch_size = numContent) for oneData in [valiData, testData]
     ]
 
     # Model Training
-    model = Transformer_RecSys_Model(transformer_model_name = "bert-base-cased").to(device)
+    model = Transformer_RecSys_Model(transformer_model_name = "bert-base-uncased").to(device)
     loss_func = nn.BCELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr = 1e-3) 
+    trainResult = DL_model_training()
 
-    # Model Evaluation（先預測後計算評估指標）
-    testYhat = DL_model_prediction(onePredictionDataLoader=testDataLoader)
+    # # Model Evaluation（先預測後計算評估指標）
+    # testYhat = DL_model_prediction(onePredictionDataLoader=testDataLoader)
