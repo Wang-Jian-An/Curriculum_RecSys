fix: decoder layer computation and transformers output

Transformer_RecSys_Model.py
@@ -8,7 +8,7 @@ class Transformer_RecSys_Model(nn.Module):
         super(Transformer_RecSys_Model, self).__init__()
         self.topicTransformerModel = AutoModel.from_pretrained(transformer_model_name)
         self.contentTransformerModel = AutoModel.from_pretrained(transformer_model_name)
-        self.decoderLayerNodes = [*[768*2/i for i in range(1, 11)], 1]
+        self.decoderLayerNodes = [*[768*2//i for i in range(1, 11)], 1]
         self.decoder = nn.Sequential(
             *[nn.Linear(self.decoderLayerNodes[nodeIndex], self.decoderLayerNodes[nodeIndex+1]) for nodeIndex in range(len(self.decoderLayerNodes)-1)],
         )
@@ -17,9 +17,9 @@ class Transformer_RecSys_Model(nn.Module):
     def forward(self, topicData, contentData):
 
         topicData = self.topicTransformerModel(input_ids = topicData[:, :, 0], 
-                                               attention_mask = topicData[:, :, 1])
+                                               attention_mask = topicData[:, :, 1])["pooler_output"]
         contentData = self.contentTransformerModel(input_ids = contentData[:, :, 0],
-                                                   attention_mask = contentData[:, :, 1])
+                                                   attention_mask = contentData[:, :, 1])["pooler_output"]
         
         interactionData = torch.cat([topicData, contentData], dim = -1)
-        return F.sigmoid(self.decoder(interactionData))
+        return torch.sigmoid(self.decoder(interactionData))[:, 0]